7f06a2ec3a6e56749f43af6b4409c1800125a6bb
Preston Macy <preston.macy@gmail.com>
2026-01-05 14:34:47 -0500

    fix: changed __dirname issue

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,9 +1,12 @@
 import path from "path";
 import os from "os";
-import { join } from "path";
+import { fileURLToPath } from "url";
 
-export const ORION_ROOT = join(__dirname, "../../");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const ORION_ROOT = path.join(__dirname, "../../");
 export const ORION_CONFIG_DIR = path.join(os.homedir(), ".config/orion");
 
 export const TFSTATE_PATH = path.join(ORION_CONFIG_DIR, "terraform.tfstate");
-export const TERRAFORM_DIR = join(ORION_ROOT, "infra/iac");
+export const TERRAFORM_DIR = path.join(ORION_ROOT, "infra/iac");
