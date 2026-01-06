c5c0b4ad4627c7ecfa84b850d0b21618682a2242
Preston Macy <preston.macy@gmail.com>
2026-01-05 22:49:46 -0500

    fix: correct config path referenced for default config file

--- src/config/paths.ts
+++ src/config/paths.ts
@@ -1,9 +1,7 @@
 import path from "path";
-import { ORION_CONFIG_DIR, ORION_ROOT } from "../shared";
+import { ORION_CONFIG_DIR } from "../shared";
+import { DEFAULT_CONFIG_PATH } from "@orion/infra";
 
 export const CONFIG_PATH = path.join(ORION_CONFIG_DIR, "config.json");
 export const CONFIG_HASH_PATH = path.join(ORION_CONFIG_DIR, "config.hash");
-export const DEFAULT_CONFIG_PATH = path.join(
-  ORION_ROOT,
-  "infra/edge/src/defaultConfig.json",
-);
+export { DEFAULT_CONFIG_PATH };
